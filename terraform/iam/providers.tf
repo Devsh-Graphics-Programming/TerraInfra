ca3aa3a2235762c63dc06861c07e4c308cccb8ca
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2025-12-14 18:42:04 +0100

    fix(iam): avoid invalid organization_id in provider

--- terraform/iam/providers.tf
+++ terraform/iam/providers.tf
@@ -1,6 +1,5 @@
 provider "scaleway" {
-  organization_id = var.organization_id
-  project_id      = var.project_id
-  zone            = var.zone
-  region          = var.region
+  project_id = var.project_id
+  zone       = var.zone
+  region     = var.region
 }
