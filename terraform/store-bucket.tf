2de93a1acc1f9f4879d1dadfdd0cf039a62e207a
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2026-04-28 00:03:18 +0200

    Allow Jenkins compare store publishing

--- terraform/store-bucket.tf
+++ terraform/store-bucket.tf
@@ -115,6 +115,7 @@ resource "scaleway_object_bucket_policy" "store" {
           "${scaleway_object_bucket.store.name}/ditt/dummy/*",
           "${scaleway_object_bucket.store.name}/ditt/public/*",
           "${scaleway_object_bucket.store.name}/ditt/private/*",
+          "${scaleway_object_bucket.store.name}/ditt/compare/*",
         ]
       },
     ]
